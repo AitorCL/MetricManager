90ed521a8a200e9abdbf13a819aea4219064ec5a
Aitor <aitor.cardona.l@gmail.com>
2013-04-16 15:26:33 +0100

    Refactoring done, new class AtributeAnalyzer created. All test passed //TODO .. Refactoring

---
+++ src/Analyzers/AtributeAnalyzer.java
@@ -0,0 +1,32 @@
+package Analyzers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class AtributeAnalyzer {
+
+    private FileParameters fileParameters;
+
+    public AtributeAnalyzer(FileParameters fileParameters) {
+        this.fileParameters = fileParameters;
+    }
+
+    public String scanForAtributes(BufferedReader bufferedFile) throws IOException {
+        String sCadena;
+        while ((sCadena = bufferedFile.readLine()) != null && sCadena.indexOf("{") == -1) {
+            bufferedFile.mark(sCadena.length());
+            isAtribute(sCadena);
+            fileParameters.increaseLineNumber();
+        }
+        return sCadena;
+    }
+
+    private void isAtribute(String sCadena) {
+        if ((sCadena.indexOf("public ") != -1
+                || sCadena.indexOf("protected ") != -1
+                || sCadena.indexOf("private ") != -1)
+                && (sCadena.endsWith(";")) && (!sCadena.endsWith(");"))) {
+            fileParameters.increaseAtributeNumber();
+        }
+    }
+}
