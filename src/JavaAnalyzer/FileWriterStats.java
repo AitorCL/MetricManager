9edb6ffd5a1a79f4f0b9e3aa75a9d5a69d06d446
Aitor <aitor.cardona.l@gmail.com>
2013-06-04 11:31:39 +0100

    Refactor done, all test passed. //TODO more test

---
+++ src/JavaAnalyzer/FileWriterStats.java
@@ -0,0 +1,62 @@
+package JavaAnalyzer;
+
+import JavaAnalyzer.ClassAnalyzer.ClassStats;
+import JavaAnalyzer.MethodAnalyzer.MethodStats;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class FileWriterStats {
+
+    public PrintWriter openLogFile(String path) throws IOException {
+        FileWriter fileLog = new FileWriter(path, true);
+        PrintWriter printWriter = new PrintWriter(fileLog, true);
+        return printWriter;
+    }
+
+    public void writePackages(ArrayList<PackageStats> packageList) throws IOException {
+        try (PrintWriter printWriterPackage = openLogFile("c:/ParseTest/Package_log.txt")) {
+            Date fecha = new Date();
+            for (PackageStats packageClass : packageList) {
+                printWriterPackage.append("-----------------------------------------------------\r\n");
+                printWriterPackage.append("[" + fecha + "]");
+                printWriterPackage.append("Name: " + packageClass.getPackageName() + ",");
+                printWriterPackage.append("Classes: " + packageClass.getPrackageClassNumber() + ",");
+                printWriterPackage.append("Lines: " + packageClass.getPackageLineNumber() + "\r\n");
+            }
+            printWriterPackage.close();
+        }
+    }
+
+    public void writeClasses(ArrayList<ClassStats> classList) throws IOException {
+        try (PrintWriter printWriterClases = openLogFile("c:/ParseTest/Class_log.txt")) {
+            Date fecha = new Date();
+            for (ClassStats actualClass : classList) {
+                printWriterClases.append("[" + fecha + "]");
+                printWriterClases.append("Name: " + actualClass.getClassName() + ",");
+                printWriterClases.append("Atributes: " + actualClass.getAtributeNumber() + ",");
+                printWriterClases.append("Methods: " + actualClass.getMethodNumber() + ",");
+                printWriterClases.append("Lines: " + actualClass.getClassLineNumber() + ",");
+                printWriterClases.append("Comment: " + actualClass.getCommentLinesNumber() + "\r\n");
+            }
+            printWriterClases.close();
+        }
+    }
+
+    public void writeMethod(ArrayList<MethodStats> methodList) throws IOException {
+        try (PrintWriter printWriterMethods = openLogFile("c:/ParseTest/Method_log.txt")) {
+            Date fecha = new Date();
+            for (MethodStats actualMethod : methodList) {
+                printWriterMethods.append("[" + fecha + "]" + "\r\n");
+                printWriterMethods.append("     class: " + actualMethod.getClassWhereIBelong() + ", ");
+                printWriterMethods.append("Method: " + actualMethod.getMethodName() + ", ");
+                printWriterMethods.append("Params: " + actualMethod.getParamNumber() + ", ");
+                printWriterMethods.append("Lines: " + actualMethod.getLineNumber() + ", ");
+                printWriterMethods.append("CC: " + actualMethod.getCyclomaticComplexity() + "\r\n");
+            }
+            printWriterMethods.close();
+        }        
+    }
+}
